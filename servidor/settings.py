4752bada41d389e0934ce9582519ddb21b4d88dd
José Vitor <masierojosevitor456@gmail.com>
2025-04-23 20:18:25 -0300

    feat

--- servidor/settings.py
+++ servidor/settings.py
@@ -103,5 +103,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
+    "http://localhost:8000",
+    "http://72.14.201.202:8000",
 ]
+CORS_ALLOW_ALL_ORIGINS = True
+
+ALLOWED_HOSTS = ['*']
