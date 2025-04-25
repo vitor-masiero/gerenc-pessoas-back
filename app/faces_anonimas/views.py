941834446e3d8c14d2f24b7f628945dc5a78cac1
José Vitor <masierojosevitor456@gmail.com>
2025-04-25 20:06:53 -0300

    feat2

--- app/faces_anonimas/views.py
+++ app/faces_anonimas/views.py
@@ -1,48 +1,16 @@
 # app/faces/views.py
-from rest_framework.views import APIView
-from rest_framework.parsers import MultiPartParser, FormParser
-from rest_framework.response import Response
-from rest_framework import status
-
+import cv2
+import tempfile
+from django.core.files import File
 from app.faces_anonimas.models import FaceAnonima
-from app.tentativas_acesso_anonimo.models import TentativaAcessoAnonimo
-from app.faces_anonimas.serializers import FaceAnonimaSerializer
-from rest_framework.generics import DestroyAPIView
 
-class FaceAnonimaRegisterView(APIView):
-    parser_classes = [MultiPartParser, FormParser]
-
-    def post(self, request, *args, **kwargs):
-        tentativa_id = request.data.get("id_tentativa_acesso_anonimo")
-        imagem = request.FILES.get("img_anonimo")
-
-        if not tentativa_id or not imagem:
-            return Response({
-                "success": False,
-                "message": "ID da tentativa e imagem são obrigatórios."
-            }, status=status.HTTP_400_BAD_REQUEST)
-
-        try:
-            tentativa = TentativaAcessoAnonimo.objects.get(pk=tentativa_id)
-        except TentativaAcessoAnonimo.DoesNotExist:
-            return Response({
-                "success": False,
-                "message": "Tentativa de acesso não encontrada."
-            }, status=status.HTTP_404_NOT_FOUND)
-
-        face_anonima = FaceAnonima.objects.create(
-            id_tentativa_acesso_anonimo=tentativa,
-            img_anonimo=imagem
+def criar_face_anonima(empresa, frame, usuario=None):
+    with tempfile.NamedTemporaryFile(suffix=".jpg") as temp_img:
+        cv2.imwrite(temp_img.name, frame)
+        temp_img.seek(0)
+        nome_arquivo = f"anonima_{usuario.id}.jpg" if usuario else "anonima_desconhecida.jpg"
+        face = FaceAnonima.objects.create(
+            id_empresa=empresa,
+            imagem=File(temp_img, name=nome_arquivo)
         )
-
-        serializer = FaceAnonimaSerializer(face_anonima)
-        return Response({
-            "success": True,
-            "data": serializer.data
-        }, status=status.HTTP_201_CREATED)
-
-
-class FaceAnonimaDeleteView(DestroyAPIView):
-    queryset = FaceAnonima.objects.all()
-    serializer_class = FaceAnonimaSerializer
-    lookup_field = 'id'
+        return face
